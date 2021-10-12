f6a705867c09d34338f24967fe9d818f56795dd9
Kunal Agarwala <kunal.agarwala6@gmail.com>
2021-10-13 00:33:16 +0100

    Add files via upload

---
+++ script/bottlesofbeer.js
@@ -0,0 +1,12 @@
+var BottlesOfBeer = 99;
+
+while(BottlesOfBeer >= 0){
+    
+    console.log(BottlesOfBeer , " bottles of beer on the wall, ", BottlesOfBeer, " bottles of beer.")
+    console.log("Take one down and pass it around, ", --BottlesOfBeer, " bottles of beer on the wall.")
+
+    BottlesOfBeer -= 1
+}
+
+console.log("No more bottles of beer on the wall, no more bottles of beer.")
+console.log("Got to the store and buy some more, 99 bottles of beer on the wall")
---
+++ script/script.js
@@ -0,0 +1,12 @@
+console.log("Kunal");
+console.info("Agarwala");
+console.warn("Hertfordshire");
+console.error("Cancer");
+
+let make = "Tesla";
+let model = "Cybertruck";
+
+console.log("My favourite car is", make, " and the model is ", model);
+
+console.log("%cHello World!", "color: orange; font-family: fantasy; font-style: bold; background-color: black; padding: 10px" );
+
